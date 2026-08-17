9513019d8a30c437bbc6b50545c0ddc6b75320c2
Alexandre Oliveira <49107539+alexandre-ronald@users.noreply.github.com>
2026-08-17 17:03:11 -0300

    Delete Sysriscos directory

--- Sysriscos/cadastro/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Organograma
-
-
-@admin.register(Organograma)
-class OrganogramaAdmin(admin.ModelAdmin):
-    list_display = ['descrição', 'sigla', ]

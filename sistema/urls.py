b7c971b159e2c45266be228e93c02c41500e2a5e
Alexandre Oliveira <49107539+alexandre-ronald@users.noreply.github.com>
2026-09-01 21:59:14 -0300

    GEN-053: adicionar rotas do Workflow Designer

--- sistema/urls.py
+++ sistema/urls.py
@@ -5,6 +5,7 @@ from . import dashboard_builder_views
 from . import form_designer_views
 from . import crud_designer_views
 from . import business_rules_views
+from . import workflow_views
 from . import validation_center_views
 from . import release_manager_views
 from . import environment_manager_views
@@ -31,6 +32,8 @@ urlpatterns = [
     path('sistemas/<int:sistema_id>/crud-designer/salvar/', crud_designer_views.salvar_crud_designer, name='salvar_crud_designer'),
     path('sistemas/<int:sistema_id>/business-rules/', business_rules_views.business_rules_designer, name='business_rules_designer'),
     path('sistemas/<int:sistema_id>/business-rules/salvar/', business_rules_views.salvar_business_rules, name='salvar_business_rules'),
+    path('sistemas/<int:sistema_id>/workflow/', workflow_views.workflow_designer, name='workflow_designer'),
+    path('sistemas/<int:sistema_id>/workflow/salvar/', workflow_views.salvar_workflows, name='salvar_workflows'),
     path('sistemas/<int:sistema_id>/validation-center/', validation_center_views.validation_center, name='validation_center'),
     path('sistemas/<int:sistema_id>/releases/', release_manager_views.release_manager, name='release_manager'),
     path('sistemas/<int:sistema_id>/releases/<int:version_id>/validar/', release_manager_views.validate_release, name='validate_release'),

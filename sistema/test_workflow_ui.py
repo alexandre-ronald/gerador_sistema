cefaaa8a2dcaa31ded0fbbd94d31664e9294377b
Alexandre Oliveira <49107539+alexandre-ronald@users.noreply.github.com>
2026-09-03 12:39:10 -0300

    GEN-057.5.2: atualizar contrato workspace do workflow

--- sistema/test_workflow_ui.py
+++ sistema/test_workflow_ui.py
@@ -61,7 +61,7 @@ class WorkflowDesignerUITests(TestCase):
         self.assertNotContains(response, "alert(")
 
     def test_workspace_lists_workflow_link(self):
-        response = self.client.get(reverse("sistema:lista"))
+        response = self.client.get(reverse("sistema:workspace", args=[self.sistema.id]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Workflow Designer")
         self.assertContains(response, self.url)
